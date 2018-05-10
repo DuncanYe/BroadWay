dc3efda61e0d2ac355888e474d9b597a69fd5ae4
Duncan <loxa10077@gmail.com>
2018-05-10 12:09:14 +0800

    add review U&D function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,45 @@
 class ReviewsController < ApplicationController
+  before_action :find_play
+  before_action :find_review, only: [:edit, :update, :destroy]
 
   def new
-    @play = Play.find(params[:play_id])
     # @review = Review.new
     @review = @play.reviews.new
   end
 
   def create
-    @play = Play.find(params[:play_id])
     @review = Review.new(review_params)
     @review.play_id = @play.id
     #  如果沒給play.id 無法存取？
     @review.user_id = current_user.id
 
     if @review.save
+      flash[:notice] = '評論已建立'
       redirect_to @play
     else
+      flash[:alert] = '評論未建立成功'
       render :new
     end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @review.update(review_params)
+      flash[:notice] = '成功更新評論'
+      redirect_to @play
+    else
+      flash[:alert] = '更新評論未完成'
+      render :edit
+    end
+  end
+
+  def destroy
+    @review.destroy
+    flash[:notice] = '評論已刪除'
+    redirect_to @play
   end
 
   private
@@ -25,4 +47,13 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :comment)
   end
+
+  def find_play
+    @play = Play.find(params[:play_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
 end
