1a89a36304ed01cfdc308d87e67e9602a4c5a552
Duncan <loxa10077@gmail.com>
2018-05-10 11:45:49 +0800

    add review R&C function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @play = Play.find(params[:play_id])
+    # @review = Review.new
+    @review = @play.reviews.new
+  end
+
+  def create
+    @play = Play.find(params[:play_id])
+    @review = Review.new(review_params)
+    @review.play_id = @play.id
+    #  如果沒給play.id 無法存取？
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to @play
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
 end
