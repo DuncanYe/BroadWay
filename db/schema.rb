28dfff87d26338ab3f11f92abed458a56e16e8f3
Duncan <loxa10077@gmail.com>
2018-05-10 10:21:31 +0800

    create revews model and review play user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_034321) do
+ActiveRecord::Schema.define(version: 2018_05_10_021750) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 2018_05_09_034321) do
     t.string "play_img_content_type"
     t.integer "play_img_file_size"
     t.datetime "play_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "play_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["play_id"], name: "index_reviews_on_play_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
